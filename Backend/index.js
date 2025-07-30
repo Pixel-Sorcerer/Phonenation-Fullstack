09fbee11d647cc42be115658ea621e5a95c5196d
unknown <bipulmahapatra2005@gmail.com>
2025-07-30 22:49:50 +0530

    Updated backend with MongoDB Atlas connection and .env setup

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/customerData")
+mongoose.connect("mongodb+srv://Nitro:Nitro%402005@cluster0.hoo8wxe.mongodb.net/")
 
 
 app.post('/signup', (req, res) => {
